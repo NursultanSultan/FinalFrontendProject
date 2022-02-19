c19c1a8010e9487aa82a4ea7685c638c6f811723
NursultanSultan <tu201906251@code.edu.az>
2022-02-19 03:35:02 -0800

    I added cinema html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+var navItem = document.getElementsByClassName("nav-link");
 window.addEventListener("scroll", () => {
     if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-        header.style.padding="10px 0";
-        header.style.backgroundColor="#17223B";
+        
+        header.style.backgroundColor="#222222";
+        header.style.opacity="1";
+        
         // headerLogo.style.width="140px";
     }else {
-        header.style.padding="15px 0";
         
-        header.style.backgroundColor="transparent";
+        header.style.backgroundColor="black";
+        header.style.opacity="0.8";
         // headerLogo.style.width="200px";
     }
 })
